66ada6a2af55fba3ecc8bfa5fd7b32186d3f9d25
selahadinnasir <selahadinas93@gmail.com>
2024-03-19 15:57:09 +0300

    added Category page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 
 import AddCategoryPage from "./compo/addCategory/page"
 
-export default function Home () {
-  return (
-    <main>
-     <AddCategoryPage />
+// Importing the function to fetch posts from the database.
+// import { fetchPosts } from "@/db/queries/posts";
+// import Link from "next/link";
+// Importing a component that handles post deletion.
+// import PostDelete from "@/components/post-delete";
 
-    
+
+export default function Home () {
+
+ return (
+    <main>
+      <AddCategoryPage/>
     </main>
     
   )
